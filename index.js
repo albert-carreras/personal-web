2fed588e51d8c15ec62e364099f32fa98e50f747
Albert Carreras <albert.carreras@mollie.com>
2020-01-27 13:52:03 +0100

    Adds sio

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
 
-app.get('/', (req, res) => res.sendFile('index.html', { root : __dirname}))
-app.get('/drip-ios', function(req, res){
-	const file = path.join(__dirname, '/upload/latest-ios.ipa');
-	res.download(file, 'latest-ios.ipa', (e) => res.send('Nothing to download yet' + e));
+app.get("/drip-ios", function(req, res) {
+  const file = path.join(__dirname, "/upload/latest-ios.ipa");
+  res.download(file, "latest-ios.ipa", e =>
+    res.send(`Nothing to download yet: ${e}`)
+  );
 });
-app.get('/drip-android', function(req, res){
-	const file = path.join(__dirname, '/upload/latest-android.apk');
-	res.download(file,'latest-android.apk', () => res.send('Nothing to download yet'));
+
+app.get("/drip-android", function(req, res) {
+  const file = path.join(__dirname, "/upload/latest-android.apk");
+  res.download(file, "latest-android.apk", () =>
+    res.send(`Nothing to download yet: ${e}`)
+  );
 });
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port);
