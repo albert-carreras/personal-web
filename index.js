cfab94b94b8ca972a55f5675362f04a0945c5b63
albert <alb.carreras@gmail.com>
2022-08-22 13:45:37 +0200

    emojis

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const helmet = require('helmet');
 const app = express();
 const port = 3000;
 
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "img-src": ["'self'", "https: data: blob:"],
+      },
+    },
+  })
+);
 
 app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
 app.get("/sitemap.xml", (req, res) =>
@@ -19,5 +29,10 @@ app.get("/robots.txt", (req, res) =>
 app.get("/cv", (req, res) =>
   res.sendFile("cv.pdf", { root: __dirname })
 );
+
+app.get("/cursor.js", (req, res) =>
+  res.sendFile(path.join(__dirname + '/cursor.js'))
+);
+
 
 app.listen(port);
